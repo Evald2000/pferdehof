2efab31bec0ae677149124471e61dd2b3e1e5ebe
Evald <e.denhof@chitinsoftware.de>
2025-01-17 19:54:39 +0100

    home seite angefangen

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,14 +1,23 @@
 import Header from "./Header";
+import hof from "./assets/Hof-aereal.jpg";
 
 const Home = () => {
     return (
         <div>
         <Header></Header>
-        <div className="mt-16">
-            <h1 >Home</h1>
-            <h1 >Home</h1>
-            <h1 >Home</h1>
-            <h1>Home</h1>
+        <div className="grid sm:grid-cols-2 gap-4 mt-28 mx-14">
+          <div className="min-h-24 rounded-lg shadow-inner bg-orange-500"><img src={hof} alt="Hof" className="w-full h-full object-cover rounded-lg" /></div>
+          <div className="min-h-24 rounded-lg shadow-inner bg-background">
+            <div className="p-10 text-lg">
+            <h1 className="font-bold text-3xl text-lightBrown">Willkommen bei Islandpferde Lechleite</h1>
+            <p>Unser Pferdehof an der schönen Lechleite liegt zwischen Friedberg und Augsburg. Wir sind spezialisiert auf Islandpferde – das sind nicht nur wunderschöne Tiere – sie eignen sich auch wunderbar als Schulpferd für Kinder und Erwachsene und begeistern sowohl Anfänger als auch anspruchsvolle Reitprofis.
+Individuellen Reitunterricht und Kurse bieten wir nicht nur für Kinder und Jugendliche an, sondern auch individuell zugeschnitten für Erwachsene. Egal, ob erfahrener Reiter, Anfänger oder Wiedereinsteiger.
+Unsere Events, Seminare und Lehrgänge rund ums Pferd finden Sie in unserem Jahresprogramm
+</p>
+</div>
+
+          </div>
+          <div></div>
         </div>
         </div>
        
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,23 +7,33 @@ module.exports = {
   theme: {
     extend: {
       keyframes: {
+        "slide-in-down": {
+          "0%": {
+            visibility: "visible",
+            transform: "translate3d(0, -15%, 0)",
+          },
+          "100%": {
+            transform: "translate3d(0, 0, 0)",
+          },
+        },
         fadeIn: {
           '0%': { opacity: 0 },
           '100%': { opacity: 1 },
         },
       },
       animation: {
+        "slide-in-down": 'slide-in-down 0.3s ease-in-out',
         fadeIn: 'fadeIn 0.3s ease-in-out',
       },
       colors: {
-        background:'#ede9e5',
+        background: '#ede9e5',
         header: '#5a4131',
         primary: '#a36314',
-        lightBrown: '#422714' 
+        lightBrown: '#422714',
       },
       fontFamily: {
-        title:["Inter","sans-serif"]
-      }
+        title: ["Inter", "sans-serif"],
+      },
     },
   },
   plugins: [],
