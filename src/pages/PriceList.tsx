0b060b389c044e3383b0643f99eb7dba3c1f13a6
Evald2000 <evalddenhof@gmail.com>
2025-01-24 12:13:39 +0100

    raw table added

--- src/pages/PriceList.tsx
+++ src/pages/PriceList.tsx
@@ -3,8 +3,25 @@ import Header from "../Header";
 const PriceList = () => {
     return (
         <div>
-            <div className="mt-20">
-            Preise und so
+            <div className="mt-32 mx-14 outline ">
+                <table className="table-auto">
+                    <thead>
+                        <tr>
+                        <th>Test</th>
+                        <th>Schulpferd</th>
+                        <th>Egenes Pferd</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Test</td>
+                            <td>250€</td>
+                            <td>200€</td>
+                            
+                        </tr>
+                    </tbody>
+                </table>
+            
             </div>
         </div>
     );
